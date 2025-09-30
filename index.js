25619f38a23d1fff0fe86c771e84a6416e21b3e7
Alcides Suarez <alcides_suarez1@hotmail.com>
2025-09-29 18:05:00 -0700

    finished whoooo

---
+++ index.js
@@ -0,0 +1,91 @@
+const BASE = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
+const COHORT = "/2508-FTB-ET-WEB-FT"; // Make sure to change this!
+const RESOURCE = "/events";
+const API = BASE + COHORT + RESOURCE;
+
+let parties = [];
+let selectedParty;
+
+async function getParty(id) {
+  try {
+    const res = await fetch(`${API}/${id}`);
+    const json = await res.json();
+    console.log(json.data);
+    selectedParty = json.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function getParties() {
+  try {
+    const res = await fetch(API);
+    const json = await res.json();
+    parties = json.data;
+    render();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function PartyListItem(party) {
+  const $li = document.createElement("li");
+  $li.innerHTML = `<a href ="#selected">${party.name}</a>`;
+  $li.addEventListener("click", async function () {
+    await getParty(party.id);
+    render();
+  });
+  return $li;
+}
+
+function PartyList() {
+  const $ul = document.createElement("ul");
+  $ul.classList.add("parties");
+  const $parties = parties.map(PartyListItem);
+  $ul.replaceChildren(...$parties);
+  return $ul;
+}
+
+function SelectedParty() {
+  if (!selectedParty) {
+    const $p = document.createElement("p");
+    $p.textContent = "Please select a party to learn more";
+    return $p;
+  }
+  const $party = document.createElement("section");
+  $party.innerHTML = `
+    <h3>${selectedParty.name} #${selectedParty.id}</h3>
+    <time datetime="${selectedParty.date}">
+    ${selectedParty.date.slice(0, 20)}</time>
+    <adress>${selectedParty.location}</adress>
+    <p>${selectedParty.description}</p>
+  `;
+  return $party;
+}
+
+function render() {
+  const $app = document.querySelector("#app");
+  $app.innerHTML = `
+    <h1>Party Planner</h1>
+    <main>
+      <section>
+      <h2>Upcoming Parties</h2>
+      <PartyList></PartyList>
+      </section>
+      <section>
+      <h2>Party Details</h2>
+      <SelectedParty></SelectedParty>
+      </section>
+    </main>
+    `;
+
+  $app.querySelector("PartyList").replaceWith(PartyList());
+  $app.querySelector("SelectedParty").replaceWith(SelectedParty());
+}
+
+async function init() {
+  await getParties();
+  render();
+}
+
+init();
---
+++ poorintent.md
@@ -0,0 +1,22 @@
+function render() {
+    const bodyHTML = document.querySelector("#app");
+    bodyHTML.innerHTML = `
+    <h1>Party Planner</h1>
+    <section class="Parties-Selection">
+     <h2>Upcoming Parties</h2>
+    <ul>
+    <li>optimize sticky deliverables</li>
+    <li>unleash extensible systems</li>
+    <li>harness synergistic e-commerce</li>
+    <li>aggregate customized partnerships</li>
+    <li>facilitate strategic platforms </li>
+    </ul>
+    </section>
+    <section>
+    <section class="info-to-details">
+    <h2>Party Details</h2>
+    </section>
+    `;
+}
+
+render();
